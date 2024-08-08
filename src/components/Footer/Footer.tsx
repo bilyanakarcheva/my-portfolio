ce276dbb1ce5440462828775475eed2c4118c3a9
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-08-08 18:33:58 +0300

    Add Footer containing project info

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer footer-center p-4 flex flex-row justify-between bg-base-200">
+            <p>© 2024 Bilyana Karcheva</p>
+            <div className="flex gap-4">
+                GitHub:
+                <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent">Bilyana Karcheva</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
