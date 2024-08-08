19d585914d56cf0cd5213457b7e03136ec6ed85b
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-08-08 18:32:55 +0300

    Add Header and navbar leading to the different pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+import { FaHome, FaClipboardList } from "react-icons/fa";
+import { PiCertificateFill } from "react-icons/pi";
+import { RiContactsBookFill } from "react-icons/ri";
+import { BsPersonVcardFill } from "react-icons/bs";
+
+
+function Header() {
+  return (
+    <div className="navbar bg-base-200 p-4 w-full">
+      <div className="flex justify-between items-center gap-8">
+        <NavLink to="/"
+          className="text-2xl font-bold text-accent flex items-center">
+          <FaHome className="mr-2" /> Home
+        </NavLink>
+        <NavLink to="/certificates"
+          className="text-2xl font-bold text-accent flex items-center">
+          <PiCertificateFill className="mr-2" />
+          Certificates
+        </NavLink>
+        <NavLink to="/portfolio"
+          className="text-2xl font-bold text-accent flex items-center">
+          <FaClipboardList className="mr-2" />
+          Portfolio
+        </NavLink>
+        <NavLink to="/about" 
+        className="text-2xl font-bold text-accent flex items-center">
+          <BsPersonVcardFill className="mr-2" />
+          About
+          </NavLink>
+        <NavLink to="/contacts"
+          className="text-2xl font-bold text-accent flex items-center">
+          <RiContactsBookFill className="mr-2" />
+          Contacts
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
