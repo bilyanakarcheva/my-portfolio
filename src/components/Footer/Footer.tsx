d183b4e99c2be65c96a77c73f7e5b83ca8721cd2
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-08-09 13:06:27 +0300

    Update github icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
+import { FaGithubSquare, FaGithub } from "react-icons/fa";
+
 function Footer() {
     return (
         <footer className="footer footer-center p-4 flex flex-row justify-between bg-base-200">
             <p>© 2024 Bilyana Karcheva</p>
             <div className="flex gap-4">
-                GitHub:
+                <FaGithubSquare size="2em" className="text-accent" />
                 <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent">Bilyana Karcheva</a>
             </div>
         </footer>
