ff522181ef3d4d4531957b80822d8438909e45d2
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-09-30 17:22:15 +0300

    Add Telerik Certificate on Certificates page

--- src/views/Certificates/Certificates.tsx
+++ src/views/Certificates/Certificates.tsx
@@ -1,8 +1,20 @@
+import telerikCertificate1 from '../../assets/Telerik_Certificate_1.png'
+import telerikCertificate2 from '../../assets/Telerik_Certificate_2.png'
+
 function Certificates() {
   return (
-    <div>
-        <h1 className="text-4xl font-bold text-accent">Certificates</h1>
+    <div className='bg-base-300 flex flex-col w-full h-auto min-h-screen rounded-lg p-20'>
+      <h1 className="text-4xl font-bold text-accent text-center mb-10">Recent Certificates</h1>
+      <div className="flex flex-row justify-center items-center gap-10">
+        <img src={telerikCertificate1} className="border rounded-lg max-w-full h-auto" style={{ maxHeight: '400px' }} alt="Certificate 1" />
+        <img src={telerikCertificate2} className="border rounded-lg max-w-full h-auto" style={{ maxHeight: '400px' }} alt="Certificate 2" />
+      </div>
+      <h1 className="text-4xl font-bold text-accent text-center p-10 mb-10">Previous Certificates</h1>
+      <div className="flex flex-row justify-center items-center gap-10">
+
+      </div>
     </div>
+
   );
 }
 
