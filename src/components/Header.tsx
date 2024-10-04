8d28264240fd8f3b072855640ad42cdf51ec6c50
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-04 19:22:49 +0300

    Adjust padding of Header elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,51 +9,51 @@ import { Link } from "react-scroll";
 
 function Header() {
   return (
-    <nav id="top" className="mb-10 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <Link
-          to="top"
-          smooth={true}
-          duration={1000}
-          spy={true}
-          className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary"
-          style={{ fontFamily: 'Charmonman, cursive'}}>
-          BK
-        </Link>
+    <nav id="top" className="bg-primary/70 flex items-center justify-between py-12">
+      <div className="px-10 flex flex-shrink-0 items-center">
+      <Link
+        to="top"
+        smooth={true}
+        duration={1000}
+        spy={true}
+        className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary"
+        style={{ fontFamily: 'Charmonman, cursive'}}>
+        BK
+      </Link>
       </div>
-        <div className="m-8 flex gap-7 text-2xl">
-            <Link
-            to="certificates"
-            smooth={true}
-            duration={1000}
-            className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
-            <PiCertificateFill className="mr-2" />
-            Certificates
-            </Link>
-          <Link
-            to="portfolio"
-            smooth={true}
-            duration={1000}
-            className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
-            <FaClipboardList className="mr-2" />
-            Portfolio
-          </Link>
-          <Link
-            to="about"
-            smooth={true}
-            duration={1000}
-            className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
-            <BsPersonVcardFill className="mr-2" />
-            About
-          </Link>
-          <Link
-            to="contact"
-            smooth={true}
-            duration={1000}
-            className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
-            <RiContactsBookFill className="mr-2" />
-            Contact
-          </Link>
+      <div className="pr-10 flex gap-7 text-2xl">
+        <Link
+        to="certificates"
+        smooth={true}
+        duration={1000}
+        className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
+        <PiCertificateFill className="mr-2" />
+        Certificates
+        </Link>
+        <Link
+        to="portfolio"
+        smooth={true}
+        duration={1000}
+        className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
+        <FaClipboardList className="mr-2" />
+        Portfolio
+        </Link>
+        <Link
+        to="about"
+        smooth={true}
+        duration={1000}
+        className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
+        <BsPersonVcardFill className="mr-2" />
+        About
+        </Link>
+        <Link
+        to="contact"
+        smooth={true}
+        duration={1000}
+        className="text-2xl font-bold text-accent flex items-center cursor-pointer hover:text-secondary">
+        <RiContactsBookFill className="mr-2" />
+        Contact
+        </Link>
       </div>
     </nav>
   )
