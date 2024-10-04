df24f3b5b98e4ad6ce6e8a6a508a3fed2129ebce
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-04 19:21:29 +0300

    Rename Home component to Hero and additionally remove padding in inner divs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
-import Home from './views/Home/Home'
+import Hero from './views/Hero/Hero'
 import Certificates from './views/Certificates/Certificates'
 import Portfolio from './views/Portfolio/Portfolio'
 import Contact from './views/Contact/Contact'
@@ -16,9 +16,9 @@ function App() {
         {/* <div className="fixed top-0 -z-10 h-full w-full">
           <div className="relative h-full w-full bg-neutral-900"><div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div></div>
         </div> */}
-        <div className="container mx-auto px-8 flex flex-col">
+        <div className="mx-auto flex flex-col">
           <Header />
-          <Home />
+          <Hero />
           <Certificates />
           <Portfolio />
           <About />
