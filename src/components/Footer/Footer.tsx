3d29199088e7e4e1abd45757661bb06519542fda
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-05 23:22:24 +0300

    Amend background color and add border to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,11 @@ import { FaGithubSquare, FaGithub } from "react-icons/fa";
 
 function Footer() {
     return (
-        <footer className="footer footer-center p-4 flex flex-row justify-between bg-base-200">
+        <footer className="footer footer-center bg-primary/80 border-t border-primary p-4 flex flex-row justify-between ">
             <p>© 2024 Bilyana Karcheva</p>
             <div className="flex gap-4">
                 <FaGithubSquare size="2em" className="text-accent" />
-                <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent">Bilyana Karcheva</a>
+                <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent font-bold">Bilyana Karcheva</a>
             </div>
         </footer>
     );
