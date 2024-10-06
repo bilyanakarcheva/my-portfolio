79968ce86ace0efc296412b0b17a41757b897566
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-06 23:15:05 +0300

    Change name of index.txs to constants.ts and add project info placeholders

--- src/constants/index.tsx
+++
@@ -1,8 +0,0 @@
-export const HERO_CONTENT = `I am a passionate software developer who recently graduated from the Alpha JavaScript program 
-in Telerik Academy, eager to dive into real-world programming challenges. With a solid foundation in modern web technologies
- and a keen eye for detail, I'm excited to turn ideas into impactful digital experiences.`;
-
- export const ABOUT_TEXT = `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`;
