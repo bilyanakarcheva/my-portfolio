24263f440eebd42273c0bbb798eb66f74dff2b5f
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-07 23:22:36 +0300

    Add Motion Framer to the Contact section

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,27 +1,57 @@
 import { FaPhoneAlt, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import { CONTACTS } from "../../constants/constants";
+import { motion } from "framer-motion";
 
 function Contact() {
     return (
         <div id="contact" className="w-full h-1/2 p-20">
-            <h1 className="text-4xl font-bold text-accent text-center mb-10">Contact me</h1>
-            <div className="flex flex-wrap gap-10 justify-center items-center bg-base-300 w-full h-1/2 rounded-lg p-20">
-                <div className="flex flex-col items-center gap-3 p-2">
+            <motion.h1
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1 }}
+                className="text-4xl font-bold text-accent text-center mb-10">
+                Contact
+                <span className="text-accent/70"> Me</span>
+            </motion.h1>
+            <div className="flex flex-wrap gap-10 text-center justify-center items-center bg-base-300 w-full h-1/2 rounded-lg p-20">
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="flex flex-col font-bold text-accent items-center gap-3 p-2">
                     <FaPhoneAlt size="2em" className="text-2xl font-bold text-accent" />
-                    <p className="font-bold text-accent">PHONE</p>
-                    <p className="font-bold text-accent">+359 878 798 207</p>
-                </div>
-                <div className="flex flex-col items-center gap-3 p-2">
+                    <p>PHONE</p>
+                    <p className="whitespace-nowrap">{CONTACTS.phone}</p>
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, y: 0 }}
+                    initial={{ opacity: 0, y: -100 }}
+                    transition={{ duration: 1 }}
+                    className="flex flex-col font-bold text-accent items-center gap-3 p-2">
                     <MdEmail size="2em" className="text-2xl font-bold text-accent" />
-                    <p className="font-bold text-accent">EMAIL</p>
-                    <p className="font-bold text-accent">bilyana.karcheva@gmail.com</p>
-                </div>
-                <div className="flex flex-col items-center gap-3 p-2">
+                    <p>EMAIL</p>
+                    <a
+                        href="mailto:{CONTACTS.email}"
+                        className="link link-accent link-hover text-accent">
+                        {CONTACTS.email}
+                    </a>
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className="flex flex-col font-bold text-accent items-center gap-3 p-2">
                     <FaLinkedin size="2em" className="text-2xl font-bold text-accent" />
-                    <p className="font-bold text-accent">LINKEDIN</p>
-                    <a href="https://www.linkedin.com/in/bilyana-karcheva-a45b26105/" className="link link-accent link-hover font-bold ">Bilyana Karcheva</a>
-                </div>
-                <a href="mailto:karcheva.bilyana@gmail.com" className="text-accent"></a>
+                    <p>LINKEDIN</p>
+                    <a
+                        href="https://www.linkedin.com/in/bilyana-karcheva-a45b26105/"
+                        className="link link-accent link-hover font-bold whitespace-nowrap"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Bilyana Karcheva
+                    </a>
+                </motion.div>
             </div>
         </div>
     );
