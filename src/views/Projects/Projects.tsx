7f57cfba06da5150b3d1f457fa3087a4da06ad86
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-12 23:45:46 +0300

    Amend size of project images and position of project information

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -13,7 +13,7 @@ function Projects() {
       </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap justify-center">
+          <div key={index} className="mb-10 flex flex-wrap justify-center">
             <div className="w-full"></div>
             <motion.img
               whileInView={{ opacity: 1, x: 0 }}
@@ -21,7 +21,7 @@ function Projects() {
               transition={{ duration: 1 }}
               src={project.image}
               height={400}
-              width={400}
+              width={500}
               alt={project.title}
               className="mb-6 rounded"
             />
@@ -29,14 +29,16 @@ function Projects() {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4">
+              className="w-full flex flex-col gap-7 justify-center items-center max-w-xl px-7 lg:w-3/4">
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-600">{project.description}</p>
-              {project.technologies.map((tech, index) => (
-                <span key={index} className="mr-2 rounded bg-neutral-500 px-2 py-1 text-sm font-medium text-orange-900">
-                  {tech}
-                </span>
-              ))}
+              <div className="flex flex-wrap">
+                {project.technologies.map((tech, index) => (
+                  <span key={index} className="m-2 rounded bg-neutral-500 px-2 py-1 text-sm font-medium text-orange-900">
+                    {tech}
+                  </span>
+                ))}
+              </div>
               {/* <div className="flex flex-wrap justify-between gap-2">
                 <a href={project.link} target="_blank" rel="noreferrer">View Project</a>
                 <a href={project.github} target="_blank" rel="noreferrer">View Code</a>
