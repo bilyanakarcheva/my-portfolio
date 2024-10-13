21166d6b142fc42ebb3e6cef5f2fbd6b4c42f4d4
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-13 13:14:54 +0300

    Add initial logic for hamburger menu for smaller devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,18 @@ import { RiContactsBookFill } from "react-icons/ri";
 import { BsPersonVcardFill } from "react-icons/bs";
 import { FaGear } from "react-icons/fa6";
 import { Link, animateScroll } from "react-scroll";
-
+import { FaGithubSquare, FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Header() {
   const [activeLink, setActiveLink] = useState('hero');
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      if(window.scrollY > 50){
+      if (window.scrollY > 50) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -29,65 +32,122 @@ function Header() {
     setActiveLink(link);
   }
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav id="top" className={`${scrolled ? 'bg-primary/80' : 'bg-primary border-b border-primary'
-    } fixed top-0 w-full flex items-center justify-between py-10 z-50`}>
-      <div className="px-10 flex flex-wrap items-center">
-      <Link
-        to="hero"
-        smooth={true}
-        duration={1000}
-        offset={-100}
-        spy={true}
-        className="text-5xl text-accent flex items-center cursor-pointer hover:text-neutral-500 font-extrabold"
-        style={{ fontFamily: 'Shadows Into Light', fontWeight: 'bolder' }}
-        onClick={() => handleActiveLink('hero')}
-        >
-        BK
-      </Link>
+      } fixed top-0 w-full items-center py-10 px-20 z-50`}>
+      <div className="flex justify-between  md:hidden sm:hidden">
+        <div className="px-10 flex flex-wrap items-center">
+          <Link
+            to="hero"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            spy={true}
+            className="text-5xl text-accent flex items-center cursor-pointer hover:text-neutral-500 font-extrabold"
+            style={{ fontFamily: 'Shadows Into Light', fontWeight: 'bolder' }}
+            onClick={() => handleActiveLink('hero')}
+          >
+            BK
+          </Link>
+        </div>
+        <div className="pr-10 flex flex-wrap gap-7 text-2xl">
+          <Link
+            to="about"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            className={`${activeLink === 'about' ? 'text-neutral-500' : 'text-accent'} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onClick={() => handleActiveLink('about')}>
+            <BsPersonVcardFill className="mr-2" />
+            About
+          </Link>
+          <Link
+            to="technologies"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            className={`${activeLink === 'technologies' ? 'text-neutral-500' : 'text-accent'} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onClick={() => setActiveLink('technologies')}>
+            <FaGear className="mr-2" />
+            Technologies
+          </Link>
+          <Link
+            to="projects"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            className={`${activeLink === 'projects' ? 'text-neutral-500' : 'text-accent '} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onClick={() => setActiveLink('projects')}>
+            <FaClipboardList className="mr-2" />
+            Projects
+          </Link>
+          <Link
+            to="contact"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            className={`${activeLink === 'contact' ? 'text-neutral-500' : 'text-accent '} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onClick={() => setActiveLink('contact')}>
+            <RiContactsBookFill className="mr-2" />
+            Contact
+          </Link>
+        </div>
+        <div className="flex justify-between gap-2 pl-4">
+          <div className="flex gap-2">
+            <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer">
+              <FaGithubSquare size="2.5em" className="text-accent cursor-pointer hover:text-neutral-500" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/bilyana-karcheva-a45b26105/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin size="2.5em" className="font-bold text-accent cursor-pointer hover:text-neutral-500" />
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="pr-10 flex flex-wrap gap-7 text-2xl">
-      <Link
-        to="about"
-        smooth={true}
-        duration={1000}
-        offset={-100}
-        className={`${activeLink === 'about' ? 'text-neutral-500' : 'text-accent'} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
-        onClick={() => handleActiveLink('about')}>
-        <BsPersonVcardFill className="mr-2" />
-        About
-      </Link>
-      <Link
-        to="technologies"
-        smooth={true}
-        duration={1000}
-        offset={-100}
-        className={`${activeLink === 'technologies' ? 'text-neutral-500' : 'text-accent'} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
-        onClick={() => setActiveLink('technologies')}>
-        <FaGear className="mr-2" />
-        Technologies
-      </Link>
-      <Link
-        to="projects"
-        smooth={true}
-        duration={1000}
-        offset={-100}
-        className={`${activeLink === 'projects' ? 'text-neutral-500' : 'text-accent '} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
-        onClick={() => setActiveLink('projects')}>
-        <FaClipboardList className="mr-2" />
-        Projects
-      </Link>
-      <Link
-        to="contact"
-        smooth={true}
-        duration={1000}
-        offset={-100}
-        className={`${activeLink === 'contact' ? 'text-neutral-500' : 'text-accent '} text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
-        onClick={() => setActiveLink('contact')}>
-        <RiContactsBookFill className="mr-2" />
-        Contact
-      </Link>
+      <div className="lg:hidden flex items-center">
+        <button onClick={toggleMenu} className="text-accent text-3xl">
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
       </div>
+      {menuOpen && (
+        <div className="absolute top-16 left-0 w-full bg-primary flex flex-col items-center gap-7 text-2xl py-4">
+          <Link
+            to="about"
+            smooth={true}
+            duration={1000}
+            offset={-80}
+            className={`${activeLink === 'about' ? 'text-neutral-500' : 'text-accent'
+              } text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onSetActive={() => {
+              setActiveLink('about');
+              setMenuOpen(false);
+            }}
+          >
+            <BsPersonVcardFill className="mr-2" />
+            About
+          </Link>
+          <Link
+            to="technologies"
+            smooth={true}
+            duration={1000}
+            offset={-80}
+            className={`${activeLink === 'technologies' ? 'text-neutral-500' : 'text-accent'
+              } text-2xl font-bold flex items-center cursor-pointer hover:text-neutral-500`}
+            onSetActive={() => {
+              setActiveLink('technologies');
+              setMenuOpen(false);
+            }}
+          >
+            Technologies
+          </Link>
+          {/* Add more links as needed */}
+        </div>
+      )}
     </nav>
   )
 }
