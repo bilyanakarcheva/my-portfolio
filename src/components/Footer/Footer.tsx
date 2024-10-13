311dbf9226199b663c73e7f67374ae522acd704b
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-13 13:15:31 +0300

    Put elements in the footer in the center

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,12 @@ import { FaGithubSquare, FaGithub } from "react-icons/fa";
 
 function Footer() {
     return (
-        <footer className="footer footer-center bg-primary border-t border-primary p-4 flex flex-row justify-between ">
+        <footer className="footer footer-center bg-primary border-t border-primary p-4 flex flex-row justify-center">
             <p>© 2024 Bilyana Karcheva</p>
-            <div className="flex gap-4">
-                <FaGithubSquare size="2em" className="text-accent" />
-                <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent font-bold">Bilyana Karcheva</a>
+            <div className="flex gap-2">
+                <a href="https://github.com/bilyanakarcheva" target="_blank" rel="noopener noreferrer" className="text-accent font-bold">
+                    <FaGithubSquare size="2.5em" className="text-accent" />
+                </a>
             </div>
         </footer>
     );
