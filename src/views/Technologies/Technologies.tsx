ec85cb099e6806b58363ba973ffcd2f57c4ade64
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-15 23:31:56 +0300

    Amend css of Technologies page to appear better on mobile screens

--- src/views/Technologies/Technologies.tsx
+++ src/views/Technologies/Technologies.tsx
@@ -30,12 +30,12 @@ const Technologies = () => {
     });
 
     return (
-        <div id="technologies" className="border-b border-base-200 pb-24">
+        <div id="technologies" className="border-b border-base-200 px-3 lg:px-10 pb-24">
             <motion.h2 
                 whileInView={ { opacity: 1, y:0 }}
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 1 }}
-            className="my-20 text-center text-4xl font-bold text-accent">
+            className="my-12 lg:my-20 text-center text-4xl font-bold text-accent">
                 Technologies
             </motion.h2>
             <motion.div
@@ -48,91 +48,91 @@ const Technologies = () => {
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <RiReactjsLine className="text-7xl text-react text-cyan-400" />
+                    <RiReactjsLine className="text-5xl lg:text-7xl text-react text-cyan-400" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(3)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <FaNodeJs className="text-7xl text-react text-green-500" />
+                    <FaNodeJs className="text-5xl lg:text-7xl text-react text-green-500" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <FaJsSquare className="text-7xl text-react text-yellow-500/70" />
+                    <FaJsSquare className="text-5xl lg:text-7xl text-react text-yellow-500/70" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(6)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <BiLogoTypescript className="text-7xl text-react text-blue-600" />
+                    <BiLogoTypescript className="text-5xl lg:text-7xl text-react text-blue-600" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(4)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <FaHtml5 className="text-7xl text-react text-red-500/80" />
+                    <FaHtml5 className="text-5xl lg:text-7xl text-react text-red-500/80" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(3.5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <IoLogoCss3 className="text-7xl text-react text-blue-500" />
+                    <IoLogoCss3 className="text-5xl lg:text-7xl text-react text-blue-500" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(2)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <RiTailwindCssFill className="text-7xl text-react text-green-600" />
+                    <RiTailwindCssFill className="text-5xl lg:text-7xl text-react text-green-600" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(1.5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <IoLogoFirebase className="text-7xl text-react text-yellow-500/90" />
+                    <IoLogoFirebase className="text-5xl lg:text-7xl text-react text-yellow-500/90" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(2.5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <FaGitAlt className="text-7xl text-react text-red-600" />
+                    <FaGitAlt className="text-5xl lg:text-7xl text-react text-red-600" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(4)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <FaGithub className="text-7xl text-react " />
+                    <FaGithub className="text-5xl lg:text-7xl text-react " />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <SiJest className="text-7xl text-react text-red-900/85" />
+                    <SiJest className="text-5xl lg:text-7xl text-react text-red-900/85" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(3)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <SiEslint className="text-7xl text-react text-indigo-600" />
+                    <SiEslint className="text-5xl lg:text-7xl text-react text-indigo-600" />
                 </motion.div>
                 <motion.div
                     variants={iconVariants(5)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-base-100 p-4">
-                    <SiVite className="text-7xl text-react text-violet-600/90" />
+                    <SiVite className="text-5xl lg:text-7xl text-react text-violet-600/90" />
                 </motion.div>
             </motion.div>
         </div>
